1df56697009688ee9542273c8ed9d87f1935216b
Yi, Huanlin <85040891+yihuanlin@users.noreply.github.com>
2023-02-02 14:04:59 +0000

    Update LibProxy.user.js

--- LibProxy.user.js
+++ LibProxy.user.js
@@ -5,8 +5,10 @@
 // @description  Redirect academic databases to libproxy.ucl.ac.uk, save your time for institutional access
 // @author       yihuanlin
 // @match        https://*.sciencedirect.com/*
-// @match        https://*.elsevierelibrary.co.uk/*
+// @match        https://elsevierelibrary.co.uk/*
 // @match        https://*.elsevier.com/*
+// @match        https://*.webofscience.com/*
+// @match        https://*.nature.com/*
 // @match        https://*.portlandpress.com/*
 // @match        https://*.biomedcentral.com/*
 // @match        https://*.wiley.com/*
@@ -17,7 +19,6 @@
 // @match        https://*.microform.digital/*
 // @match        https://*.microbiologyresearch.org/*
 // @match        https://*.asm.org/*
-// @match        https://*.webofscience.com/*
 // @match        https://scholar.google.com/*
 // @match        https://*.royalsociety.org/*
 // @match        https://*.rsc.org/*
@@ -27,10 +28,8 @@
 // @match        https://iopscience.iop.org/*
 // @match        https://*.bmj.com/*
 // @match        https://*.frontiersin.org/*
-// @match        https://*.jneurosci.org/*
 // @match        https://*.mhmedical.com/*
 // @match        https://*.thejns.org/*
-// @match        https://*.nature.com/*
 // @match        https://*.science.org/*
 // @match        https://*.nejm.org/*
 // @match        https://*.annualreviews.org/*
@@ -45,6 +44,10 @@
 // @match        https://*.ahajournals.org/*
 // @match        https://*.psychologicalscience.org/*
 // @match        https://www.tandfonline.com/*
+// @match        https://*.apa.org/*
+// @match        https://*.doi.org/*
+// @match        https://ascelibrary.org/*
+// @match        https://pubmed.ncbi.nlm.nih.gov/*
 // @icon         https://cdn.ucl.ac.uk/indigo/images/favicon.ico
 // @grant        none
 // ==/UserScript==
